028d31e4921ba68dfe7978e7210781f6ba66aad5
tintinweb <tintinweb@oststrom.com>
2020-05-13 16:18:46 +0200

    fix multiroot prepare v0.0.8

--- src/extension.js
+++ src/extension.js
@@ -6,15 +6,15 @@
 
 
 /** imports */
-const vscode = require("vscode")
+const vscode = require("vscode");
 const path = require('path');
 
-const truffleFlattener = require("truffle-flattener")
-const findUp = require("find-up")
+const truffleFlattener = require("truffle-flattener");
+const findUp = require("find-up");
 
 /** global vars */
-const LANGID = 'solidity'
-const EXTENSION_PREFIX = 'vscode-solidity-flattener'
+const LANGID = 'solidity';
+const EXTENSION_PREFIX = 'vscode-solidity-flattener';
 
 //const config = vscode.workspace.getConfiguration(EXTENSION_PREFIX);
 
@@ -23,12 +23,12 @@ const EXTENSION_PREFIX = 'vscode-solidity-flattener'
 
 /** funcdecs */
 
-function newWindowBeside(content){
-    vscode.workspace.openTextDocument({content: content, language: LANGID})
-        .then(doc => vscode.window.showTextDocument(doc, vscode.ViewColumn.Beside))
+function newWindowBeside(content) {
+    vscode.workspace.openTextDocument({ content: content, language: LANGID })
+        .then(doc => vscode.window.showTextDocument(doc, vscode.ViewColumn.Beside));
 }
 
-function isSubpath(parent, dir){
+function isSubpath(parent, dir) {
     const relative = path.relative(parent, dir);
     return relative && !relative.startsWith('..') && !path.isAbsolute(relative);
 }
@@ -36,79 +36,89 @@ function isSubpath(parent, dir){
 
 /** event funcs */
 function onActivate(context) {
-    
+
     context.subscriptions.push(
         vscode.commands.registerCommand(EXTENSION_PREFIX + '.activeFile.flatten', (args) => {
 
-            let activeFile = vscode.window.activeTextEditor.document.fileName
-            if(activeFile.endsWith(".sol")){
-                findUp(["truffle.js", "truffle-config.js"],{"cwd":path.dirname(activeFile)}).then(tpath => {
-                    if(!isSubpath(vscode.workspace.rootPath, tpath)){
-                        vscode.window.showErrorMessage('[Flatten failed] ' + x + "\n" + "The contract does not appear to be part of a truffle project.")
+            let activeDoc = vscode.window.activeTextEditor.document;
+            let activeFile= activeDoc.fileName;
+
+            let workspacePath = vscode.workspace.getWorkspaceFolder(activeDoc.uri);
+
+            if (activeFile.endsWith(".sol")) {
+                findUp(["truffle.js", "truffle-config.js"], { "cwd": path.dirname(activeFile) }).then(tpath => {
+                    if (!workspacePath || !isSubpath(workspacePath.uri.fsPath, tpath)) {
+                        vscode.window.showErrorMessage(`[Flatten failed] ${activeFile}\nThe contract does not appear to be part of a truffle project.`);
                         return;
                     }
-                    
-                    truffleFlattener([activeFile], path.dirname(tpath)).then(res => newWindowBeside(res)).then( () => {
-                        vscode.window.showInformationMessage('[Flatten success] ' + activeFile)
+
+                    truffleFlattener([activeFile], path.dirname(tpath)).then(res => newWindowBeside(res)).then(() => {
+                        vscode.window.showInformationMessage(`[Flatten success]  ${activeFile}`);
                     }).catch(ex => {
-                        vscode.window.showErrorMessage('[Flatten failed] ' + activeFile + "\n" + ex.message + "\n\n" + "NOTE: Please make sure to run `npm install` in the truffle project dir.")
-                        console.error(ex)
-                    })
-                })
+                        vscode.window.showErrorMessage(`[Flatten failed] ${activeFile}\n${ex.message}\n\nNOTE: Please make sure to run \`npm install\` in the truffle project dir.`);
+                        console.error(ex);
+                    });
+                });
             }
         })
-    )
+    );
 
     context.subscriptions.push(
         vscode.commands.registerCommand(EXTENSION_PREFIX + '.contextMenu.flatten', async (clickedFile, selectedFiles) => {
 
-            selectedFiles.map(x => x.fsPath).filter(x => x.endsWith(".sol")).forEach(x => {
-                findUp(["truffle.js", "truffle-config.js"],{"cwd":path.dirname(x)}).then(tpath => {
-                    if(!isSubpath(vscode.workspace.rootPath, tpath)){
-                        vscode.window.showErrorMessage('[Flatten failed] ' + x + "\n" + "The contract does not appear to be part of a truffle project.")
+            selectedFiles.filter(x => x.fsPath.endsWith(".sol")).forEach(x => {
+                findUp(["truffle.js", "truffle-config.js"], { "cwd": path.dirname(x.fsPath) }).then(tpath => {
+
+                    let workspacePath = vscode.workspace.getWorkspaceFolder(x);
+
+                    if (!workspacePath || !isSubpath(workspacePath.uri.fsPath, tpath)) {
+                        vscode.window.showErrorMessage(`[Flatten failed] ${x.fsPath}\nThe contract does not appear to be part of a truffle project.`);
                         return;
                     }
 
-                    truffleFlattener([x], path.dirname(tpath)).then(res => newWindowBeside(res)).then( () => {
-                        vscode.window.showInformationMessage('[Flatten success] ' + x)
+                    truffleFlattener([x.fsPath], path.dirname(tpath)).then(res => newWindowBeside(res)).then(() => {
+                        vscode.window.showInformationMessage(`[Flatten success] ${x.fsPath}`);
                     }).catch(ex => {
-                        vscode.window.showErrorMessage('[Flatten failed] ' + x + "\n" + ex.message + "\n\n" + "NOTE: Please make sure to run `npm install` in the truffle project dir.")
-                        console.error(ex)
-                    })
-                })
-            })
+                        vscode.window.showErrorMessage(`[Flatten failed] ${x.fsPath}\n${ex.message}\n\nNOTE: Please make sure to run \`npm install\` in the truffle project dir.`);
+                        console.error(ex);
+                    });
+                });
+            });
 
         })
-    )
+    );
 
     context.subscriptions.push(
         vscode.commands.registerCommand(EXTENSION_PREFIX + '.flatten', async (args) => {
             // take document or string; default active editor if
-            args = args || {}
+            args = args || {};
             let options = {
                 files: args.files,
                 showErrors: args.showErrors === undefined ? true : showErrors,
-                callback: args.callback || function(file, tpath, res){ newWindowBeside(res)}
-            }
+                callback: args.callback || function (file, tpath, res) { newWindowBeside(res); }
+            };
 
-            options.files.map(x => x.fsPath).filter(x => x.endsWith(".sol")).forEach(x => {
-                findUp(["truffle.js", "truffle-config.js"],{"cwd":path.dirname(x)}).then(tpath => {
-                    if(!isSubpath(vscode.workspace.rootPath, tpath)){
-                        options.showErrors && vscode.window.showErrorMessage('[Flatten failed] ' + x + "\n" + "The contract does not appear to be part of a truffle project.")
+            options.files.filter(x => x.fsPath.endsWith(".sol")).forEach(x => {
+                findUp(["truffle.js", "truffle-config.js"], { "cwd": path.dirname(x.fsPath) }).then(tpath => {
+
+                    let workspacePath = vscode.workspace.getWorkspaceFolder(x);
+
+                    if (!workspacePath || !isSubpath(workspacePath.uri.fsPath, tpath)) {
+                        options.showErrors && vscode.window.showErrorMessage(`[Flatten failed] ${x.fsPath}\nThe contract does not appear to be part of a truffle project.`);
                         return;
                     }
 
-                    truffleFlattener([x], path.dirname(tpath)).then(res => options.callback(x, tpath, res)).then( () => {
-                        options.showErrors && vscode.window.showInformationMessage('[Flatten success] ' + x)
+                    truffleFlattener([x.fsPath], path.dirname(tpath)).then(res => options.callback(x.fsPath, tpath, res)).then(() => {
+                        options.showErrors && vscode.window.showInformationMessage(`[Flatten success] ${x.fsPath}`);
                     }).catch(ex => {
-                        options.showErrors && vscode.window.showErrorMessage('[Flatten failed] ' + x + "\n" + ex.message + "\n\n" + "NOTE: Please make sure to run `npm install` in the truffle project dir.")
-                        console.error(ex)
-                    })
-                })
-            })
+                        options.showErrors && vscode.window.showErrorMessage(`[Flatten failed] ${x.fsPath} \n${ex.message}\n\nNOTE: Please make sure to run \`npm install\` in the truffle project dir.`);
+                        console.error(ex);
+                    });
+                });
+            });
 
         })
-    )
+    );
 }
 
 /* exports */
